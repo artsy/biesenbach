205ec70faf89bb5d27acd2a1461a9a33c4ff25fe
dblock <dblock@dblock.org>
2017-02-27 09:38:01 -0500

    Connect welcome intent wiht text intent asking questions. Respond with a final piece of information, don't ask a question.

--- test/test_action.js
+++ test/test_action.js
@@ -30,11 +30,36 @@ describe('Artsy', () => {
       .post('/')
       .send({
         inputs: [{
-          intent: 'assistant.intent.action.MAIN'
+          intent: 'assistant.intent.action.MAIN',
+          raw_inputs: [{
+            input_type: 2,
+            query: "OK Google, talk to Artsy."
+          }]
         }]
       })
       .expect(200).then((response) => {
-        expect(response.body.expected_inputs[0].input_prompt.initial_prompts[0].ssml).to.eql(`Welcome to Artsy!`);
+        expect(response.body.expected_inputs[0].input_prompt.initial_prompts[0].ssml).to.eql(`What is the name of the artist you would like to hear about?`);
+      });
+  });
+
+  it('talks about an artist when launched', () => {
+    return request(action)
+      .post('/')
+      .send({
+        inputs: [{
+          intent: 'assistant.intent.action.MAIN',
+          raw_inputs: [{
+            input_type: 2,
+            query: "OK Google, Ask Artsy about andy warhol."
+          }],
+          arguments: []
+        }]
+      })
+      .expect(200).then((response) => {
+        expect(response.body.expect_user_response).to.equal(false);
+        var ssml = response.body.final_response.speech_response.text_to_speech;
+        expect(ssml).to.startWith('American artist Andy Warhol was born in Pittsburgh in 1928 and died in 1987. Obsessed with celebrity, ');
+        expect(ssml).to.endWith('silk-screen printmaking to achieve his characteristic hard edges and flat areas of color.');
       });
   });
 
@@ -52,7 +77,8 @@ describe('Artsy', () => {
         }]
       })
       .expect(200).then((response) => {
-        var ssml = response.body.expected_inputs[0].input_prompt.initial_prompts[0].ssml;
+        expect(response.body.expect_user_response).to.equal(false);
+        var ssml = response.body.final_response.speech_response.text_to_speech;
         expect(ssml).to.startWith('American artist Andy Warhol was born in Pittsburgh in 1928 and died in 1987. Obsessed with celebrity, ');
         expect(ssml).to.endWith('silk-screen printmaking to achieve his characteristic hard edges and flat areas of color.');
       });
